503caf5098429b22ba6beb9fb4f0bd3a50eef18a
philomath213 <bilalphilomath@gmail.com>
2019-12-02 00:41:41 +0100

    logging info

--- bilucci_bot.py
+++ bilucci_bot.py
@@ -11,6 +11,13 @@ from telegram.ext import (
 # BOT_API_TOKEN
 BOT_API_TOKEN = os.environ.get("BOT_API_TOKEN", None)
 
+logging.basicConfig(
+    format='[+] [%(asctime)s] %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger()
+
 # Spoils list
 SPOILS = [
     "No spoilers for the moment"
@@ -27,35 +34,43 @@ QUOTES = [
 
 
 def start_spoiling(update, context):
+    user = update.effective_user
+    logger.info(f"{user.username} triggers start_spoiling")
+
     spoil = choice(SPOILS)
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=spoil
     )
 
+    logger.info(f"start_spoiling: {spoil}")
+
 
 def gimme_quote(update, context):
+    user = update.effective_user
+    logger.info(f"{user.username} triggers gimme_quote")
+
     quote = choice(QUOTES)
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=quote
     )
+    logger.info(f"gimme_quote: {quote}")
 
 
 def main():
-    logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO
-    )
-
     updater = Updater(token=BOT_API_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
     start_spoiling_handler = CommandHandler('start_spoiling', start_spoiling)
     dispatcher.add_handler(start_spoiling_handler)
 
+    logger.info("add 'start_spoiling' handler")
+
     gimme_quote_handler = CommandHandler('gimme_quote', gimme_quote)
     dispatcher.add_handler(gimme_quote_handler)
+
+    logger.info("add 'gimme_quote' handler")
 
     updater.start_polling()
 
