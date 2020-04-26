60e9da1d183ba23f3f0cc14470e724d28540a130
philomath213 <bilalphilomath@gmail.com>
2020-04-26 18:55:37 +0100

    add module main

---
+++ bilucci/__init__.py
@@ -0,0 +1,4 @@
+from .bilucci_bot import main
+from .version import __version__
+
+__all__ = ["main", "__version__"]
--- bilucci/bilucci_bot.py
+++ bilucci/bilucci_bot.py
@@ -211,7 +211,3 @@ def main():
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=BOT_API_TOKEN)
     updater.bot.set_webhook(WEBHOOK_URL)
     updater.idle()
-
-
-if __name__ == "__main__":
-    main()
