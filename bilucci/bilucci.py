43d8213231b1b0a6fc4441c191457cd03a0a3ac6
philomath213 <bilalphilomath@gmail.com>
2020-04-26 18:40:33 +0100

    linting and formating using black

--- bilucci/bilucci.py
+++ bilucci/bilucci.py
@@ -2,20 +2,15 @@ import os
 import logging
 from random import choice
 
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-)
-
+from telegram.ext import Updater, CommandHandler
 import datamuse
-
 import omdb
 
 
 # PORT
-PORT = int(os.environ.get('PORT', '8443'))
+PORT = int(os.environ.get("PORT", "8443"))
 # Heroku Application Name
-APP_NAME = os.environ.get('APP_NAME', None)
+APP_NAME = os.environ.get("APP_NAME", None)
 # BOT_API_TOKEN
 BOT_API_TOKEN = os.environ.get("BOT_API_TOKEN", None)
 
@@ -25,20 +20,15 @@ WEBHOOK_URL = f"https://{APP_NAME}.herokuapp.com/{BOT_API_TOKEN}"
 OMDB_API_KEY = os.environ.get("OMDB_API_KEY", None)
 
 # setup 3rd party APIs
-omdb.set_default('apikey', OMDB_API_KEY)
+omdb.set_default("apikey", OMDB_API_KEY)
 datamuse_api = datamuse.Datamuse()
 
-logging.basicConfig(
-    format='[+] [%(asctime)s] %(message)s',
-    level=logging.INFO
-)
+logging.basicConfig(format="[+] [%(asctime)s] %(message)s", level=logging.INFO)
 
 logger = logging.getLogger()
 
 # Spoils list
-SPOILS = [
-    "No spoilers for the moment"
-]
+SPOILS = ["No spoilers for the moment"]
 
 # Quotes list
 QUOTES = [
@@ -70,10 +60,7 @@ def start_spoiling(update, context):
     logger.info(f"{user.username} triggers start_spoiling")
 
     spoil = choice(SPOILS)
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=spoil
-    )
+    context.bot.send_message(chat_id=update.effective_chat.id, text=spoil)
 
     logger.info(f"start_spoiling: {spoil}")
 
@@ -83,10 +70,7 @@ def gimme_quote(update, context):
     logger.info(f"{user.username} triggers gimme_quote")
 
     quote = choice(QUOTES)
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=quote
-    )
+    context.bot.send_message(chat_id=update.effective_chat.id, text=quote)
     logger.info(f"gimme_quote: {quote}")
 
 
@@ -99,16 +83,13 @@ def synonyms(update, context):
 
         syns = datamuse_api.words(rel_syn=word, max=10)
         if syns:
-            msg = '\n'.join(map(lambda s: s['word'], syns))
+            msg = "\n".join(map(lambda s: s["word"], syns))
         else:
             msg = f"can't find synonyms for {word}"
     else:
         msg = "/synonyms <word>"
 
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=msg
-    )
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
     logger.info("synonyms: %r" % msg)
 
@@ -118,10 +99,7 @@ def good_night_alphabit(update, context):
     logger.info(f"{user.username} triggers good_night")
 
     good_night_msg = choice(GOOD_NIGHT_ALPHABIT_MSG)
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=good_night_msg
-    )
+    context.bot.send_message(chat_id=update.effective_chat.id, text=good_night_msg)
     logger.info(f"good_night_alphabit: {good_night_msg}")
 
 
@@ -130,10 +108,7 @@ def good_night_ingeniums(update, context):
     logger.info(f"{user.username} triggers good_night")
 
     good_night_msg = choice(GOOD_NIGHT_INGENIUMS_MSG)
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=good_night_msg
-    )
+    context.bot.send_message(chat_id=update.effective_chat.id, text=good_night_msg)
     logger.info(f"good_night_ingeniums: {good_night_msg}")
 
 
@@ -142,10 +117,7 @@ def esi_cinema(update, context):
     logger.info(f"{user.username} triggers esi_cinema")
 
     esi_cinema_msg = choice(ESI_CINAME_MSG)
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=esi_cinema_msg
-    )
+    context.bot.send_message(chat_id=update.effective_chat.id, text=esi_cinema_msg)
     logger.info(f"esi_cinema: {esi_cinema_msg}")
 
 
@@ -153,14 +125,14 @@ def what_movie(update, context):
     user = update.effective_user
 
     if context.args:
-        title = ' '.join(context.args).strip()
+        title = " ".join(context.args).strip()
         logger.info(f"{user.username} triggers what_movie: {title}")
 
         movie_info = omdb.request(t=title)
         if movie_info:
             msg = ""
             for x, y in movie_info.json().items():
-                if x == 'Ratings':
+                if x == "Ratings":
                     ratings = "Ratings:\n"
                     for r in y:
                         ratings += f"    {r['Source']}: {r['Value']}\n"
@@ -172,10 +144,7 @@ def what_movie(update, context):
     else:
         msg = "/what_movie <title>"
 
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=msg
-    )
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
     logger.info("what_movie: %r" % msg)
 
@@ -197,54 +166,49 @@ def help_cmd(update, context):
     for command in commands:
         text += command[0] + " " + command[1] + "\n"
 
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=text
-    )
+    context.bot.send_message(chat_id=update.message.chat_id, text=text)
 
 
 def main():
     updater = Updater(token=BOT_API_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
-    help_cmd_handler = CommandHandler('help', help_cmd)
+    help_cmd_handler = CommandHandler("help", help_cmd)
     dispatcher.add_handler(help_cmd_handler)
     logger.info("add 'help_cmd' handler")
 
-    start_spoiling_handler = CommandHandler('start_spoiling', start_spoiling)
+    start_spoiling_handler = CommandHandler("start_spoiling", start_spoiling)
     dispatcher.add_handler(start_spoiling_handler)
     logger.info("add 'start_spoiling' handler")
 
-    gimme_quote_handler = CommandHandler('gimme_quote', gimme_quote)
+    gimme_quote_handler = CommandHandler("gimme_quote", gimme_quote)
     dispatcher.add_handler(gimme_quote_handler)
     logger.info("add 'gimme_quote' handler")
 
-    synonyms_handler = CommandHandler('synonyms', synonyms)
+    synonyms_handler = CommandHandler("synonyms", synonyms)
     dispatcher.add_handler(synonyms_handler)
     logger.info("add 'synonyms' handler")
 
-    good_night_alphabit_handler = CommandHandler('good_night_alphabit', good_night_alphabit)
+    good_night_alphabit_handler = CommandHandler("good_night_alphabit", good_night_alphabit)
     dispatcher.add_handler(good_night_alphabit_handler)
     logger.info("add 'good_night_alphabit' handler")
 
-    good_night_ingeniums_handler = CommandHandler('good_night_ingeniums', good_night_ingeniums)
+    good_night_ingeniums_handler = CommandHandler("good_night_ingeniums", good_night_ingeniums)
     dispatcher.add_handler(good_night_ingeniums_handler)
     logger.info("add 'good_night_ingeniums' handler")
 
-    esi_cinema_handler = CommandHandler('esi_cinema', esi_cinema)
+    esi_cinema_handler = CommandHandler("esi_cinema", esi_cinema)
     dispatcher.add_handler(esi_cinema_handler)
     logger.info("add 'esi_cinema' handler")
 
-    what_movie_handler = CommandHandler('what_movie', what_movie)
+    what_movie_handler = CommandHandler("what_movie", what_movie)
     dispatcher.add_handler(what_movie_handler)
     logger.info("add 'what_movie' handler")
 
     logger.info(f"PORT: {PORT}")
     logger.info(f"WEBHOOK_URL: {WEBHOOK_URL}")
 
-    updater.start_webhook(listen="0.0.0.0",
-                          port=PORT,
-                          url_path=BOT_API_TOKEN)
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=BOT_API_TOKEN)
     updater.bot.set_webhook(WEBHOOK_URL)
     updater.idle()
 
